f121a97192020a3ecc7c7dd37c78a567867899ec
Gerard Paligot <gerard.paligot@gmail.com>
2022-03-15 23:45:10 +0100

    feat: Add dark theme.

--- app/src/main/java/app/shopping/a11y/MainActivity.kt
+++ app/src/main/java/app/shopping/a11y/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.primarySurface
+import androidx.compose.runtime.SideEffect
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import app.shopping.a11y.ui.screens.list.ProductListVM
 import app.shopping.a11y.ui.theme.A11yShoppingAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
@@ -13,7 +20,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             A11yShoppingAppTheme {
-                ProductListVM()
+                val systemUiController = rememberSystemUiController()
+                val useDarkIcons = MaterialTheme.colors.isLight
+                val navBarColor = MaterialTheme.colors.surface
+                SideEffect {
+                    systemUiController.setSystemBarsColor(color = navBarColor, darkIcons = useDarkIcons)
+                    systemUiController.setNavigationBarColor(color = navBarColor, darkIcons = useDarkIcons)
+                }
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "products") {
+                    composable(route = "products") {
+                        ProductListVM()
+                    }
+                }
             }
         }
     }
