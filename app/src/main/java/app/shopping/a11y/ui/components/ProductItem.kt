56779e8e04db1b6b926d81cd4c9ee3a2d267ab49
Gerard Paligot <gerard.paligot@gmail.com>
2022-03-17 21:50:54 +0100

    feat: Add product image in cells.

--- app/src/main/java/app/shopping/a11y/ui/components/ProductItem.kt
+++ app/src/main/java/app/shopping/a11y/ui/components/ProductItem.kt
@@ -1,12 +1,14 @@
 package app.shopping.a11y.ui.components
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,10 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import app.shopping.a11y.R
 import app.shopping.a11y.data.ProductUi
 import app.shopping.a11y.ui.theme.A11yShoppingAppTheme
+import coil.compose.AsyncImage
 
 @ExperimentalAnimationApi
 @Composable
@@ -28,7 +33,17 @@ fun ProductItem(
     onRemoveQuantityClicked: (productId: String) -> Unit,
     onAddQuantityClicked: (productId: String) -> Unit,
 ) {
-    Row(modifier = modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = product.url,
+            contentDescription = "Product Image",
+            placeholder = painterResource(id = R.drawable.placeholder),
+            modifier = Modifier.size(80.dp)
+        )
         Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
             Text(text = product.name)
             Box(
